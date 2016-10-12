a0a1f421881768a26995b5c0a15180fa6d520b98
Giulia Frazzetta <giulia.frazzetta@gmail.com>
2016-10-12 15:24:12 -0400

    Main changed to take metaData struct as input.

--- analysis/pupilPETD_main.m
+++ analysis/pupilPETD_main.m
@@ -1,4 +1,4 @@
-function [response] = pupilPETD_main(someInput)
+function [response] = pupilPETD_main(metaData)
 
 % Inputs:
 % paths
@@ -16,14 +16,32 @@ function [response] = pupilPETD_main(someInput)
 %     GazeCalName
 
 %% load in all data
+% Get user name and define path to dropbox folder
+[~, tmpName] = system('whoami');
+userName = strtrim(tmpName);
+dropboxDir = ['/Users/' userName '/Dropbox-Aguirre-Brainard-Lab/'];
+% define eyetracking data path
+if isfield(metaData.dropboxPaths, 'projectSubfolder')
+    dataPath =  (fullfile(dropboxDir,metaData.dropboxPaths.projectSubfolder, ...
+        metaData.names.subjectName,metaData.names.sessionDate,'Eyetracking'));
+else
+    dataPath =  (fullfile(dropboxDir,metaData.dropboxPaths.projectFolder, ...
+        metaData.names.subjectName,metaData.names.sessionDate,'Eyetracking'));
+end
+% load
+load (fullfile(dataPath,[metaData.names.runName '_report.mat']));
+load (fullfile(dataPath,[metaData.names.ScaleCalName '.mat']));
+if isfield (metaData.names, 'GazeCalName')
+    load (fullfile(dataPath,[metaData.names.GazeCalName '.mat']));
+end
 
-
-%% make metaData
-
-[metaData] = makeMetaData(someInput);
 %% Calibrate LiveTrack data
-
-[PupilData] = CalibrateLivetrackData(Report,ScaleCal,CalMat,Rpc,viewDist);
+if isfield (metaData.names, 'GazeCalName')
+    viewDist = metaData.refValues.screenSpecs.viewDist;
+    [PupilData] = CalibrateLivetrackData(Report,ScaleCal,CalMat,Rpc,viewDist);
+else
+    [PupilData] = CalibrateLivetrackData(Report,ScaleCal);
+end
 %% Make response structure
 
 [response] = MakePupilResponseStruct(PupilData, metaData);
